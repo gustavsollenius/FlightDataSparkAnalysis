5be2324802b0f501e7e8021265cc1c9486712598
gustavsollenius <your_email@example.com>
2024-05-03 19:55:43 +0200

    update

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pyspark.sql import *
 spark = SparkSession.builder.appName("App").master("local[*]").getOrCreate()
-
+spark.sp
 path = "flights_data/flights.csv"
 flightsdf = spark.read.csv(path,header=True,inferSchema=True)
 
-print(flightsdf.show(20))
+print(flightsdf.filter(flightsdf.CANCELLED != 0).show(10))
